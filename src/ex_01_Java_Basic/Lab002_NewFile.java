c1f23e6e1ecad313aa29d056b2929c510ac635e7
jkodati <jagannathkodati@gmail.com>
2025-01-10 09:32:11 +0530

    4th commit

---
+++ src/ex_01_Java_Basic/Lab002_NewFile.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab002_NewFile {
+    public static void main(String[] args) {
+        System.out.println("New File");
+        System.out.println("I am the best");
+    }
+}
