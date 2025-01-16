45e71a2f72de57e08277214254b582137205fdea
jkodati <jagannathkodati@gmail.com>
2025-01-16 23:46:36 +0530

    5th commit

---
+++ src/ex_01_Java_Basic/Lab004_Printf_Task.java
@@ -0,0 +1,26 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Printf_Task {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf("9*1=%d",num*1);
+        System.out.println();
+        System.out.printf("9*2=%d",num*2);
+        System.out.println();
+        System.out.printf("9*3=%d",num*3);
+        System.out.println();
+        System.out.printf("9*4=%d",num*4);
+        System.out.println();
+        System.out.printf("9*5=%d",num*5);
+        System.out.println();
+        System.out.printf("9*6=%d",num*6);
+        System.out.println();
+        System.out.printf("9*7=%d",num*7);
+        System.out.println();
+        System.out.printf("9*8=%d",num*8);
+        System.out.println();
+        System.out.printf("9*9=%d",num*9);
+        System.out.println();
+        System.out.printf("9*10=%d",num*10);
+    }
+}
